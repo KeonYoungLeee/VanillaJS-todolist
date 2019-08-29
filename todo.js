23a64f2902cff7eba2ae7faa861b2209c44b3de8
KeonYoungLeee <37015435+KeonYoungLeee@users.noreply.github.com>
2019-08-30 01:03:00 +0900

    Add files via upload

--- todo.js
+++ todo.js
@@ -4,15 +4,45 @@ const toDoList = document.querySelector('.js-toDoList');
 
 const TODOS_LS = 'toDos';
 
+let toDos = [];
+
+
+
+const deleteToDos = (event) => {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    // It eliminates the same thing
+    // Do not delete if 'li' is different
+    const cleanToDos = toDos.filter((todo)  => {
+        return todo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+const saveToDos = () => {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 const paintToDO = (text) => {
     const li = document.createElement('li');
     const delBtn = document.createElement('button');
-    delBtn.innerText = "❌";
     const span = document.createElement('span');
+    const newId = toDos.length + 1;
+    delBtn.innerText = "❌";
+    delBtn.addEventListener('click', deleteToDos);
     span.innerText = text;
-    li.appendChild(span);
     li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id:newId,
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 const todohandlerSubmit = (event) => {
@@ -23,10 +53,15 @@ const todohandlerSubmit = (event) => {
    
 }
 
-const loadToDos =()=> {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
 
+
+const loadToDos =()=> {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(todo => {
+            paintToDO(todo.text);
+        });
     } 
 }
 
